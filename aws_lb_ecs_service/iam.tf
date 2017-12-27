1d558576b835a385bbf70202c867995c5dcbf440
halfb00t <halfb00t@gmail.com>
2017-12-27 21:42:11 +0600

    Fixed iam role for ecs service

--- aws_lb_ecs_service/iam.tf
+++ aws_lb_ecs_service/iam.tf
@@ -1,48 +1,23 @@
 resource "aws_iam_role" "main" {
-  name = "${var.service_name}-ecs-service-role"
+  name = "${var.name}-ecs-service-role"
 
-  assume_role_policy = <<EOF
+   assume_role_policy = <<EOF
 {
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Action": "sts:AssumeRole",
-            "Principal": {"AWS": "*"},
-            "Effect": "Allow",
-            "Sid": ""
-        }
-    ]
-}
-EOF
-}
-
-resource "aws_iam_role_policy" "main" {
-  name = "${var.service_name}-ecs-service-policy"
-  role = "${aws_iam_role.main.name}"
-
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
+  "Version": "2008-10-17",
   "Statement": [
     {
-      "Effect": "Allow",
-      "Action": [
-        "ec2:AttachNetworkInterface",
-        "ec2:CreateNetworkInterface",
-        "ec2:CreateNetworkInterfacePermission",
-        "ec2:DeleteNetworkInterface",
-        "ec2:DeleteNetworkInterfacePermission",
-        "ec2:Describe*",
-        "ec2:DetachNetworkInterface",
-        "elasticloadbalancing:DeregisterInstancesFromLoadBalancer",
-        "elasticloadbalancing:DeregisterTargets",
-        "elasticloadbalancing:Describe*",
-        "elasticloadbalancing:RegisterInstancesWithLoadBalancer",
-        "elasticloadbalancing:RegisterTargets"
-      ],
-      "Resource": "*"
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ecs.amazonaws.com"]
+      },
+      "Effect": "Allow"
     }
   ]
 }
 EOF
+}
+
+resource "aws_iam_role_policy_attachment" "main" {
+  role       = "${aws_iam_role.main.id}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
 }

21737be358fd0e8e592d885dbe8e883d6d8d58b9
halfb00t <halfb00t@gmail.com>
2017-12-28 17:55:33 +0600

    Fixed module outputs

--- aws_lb_ecs_service/outputs.tf
+++ aws_lb_ecs_service/outputs.tf
@@ -1,11 +1,11 @@
 output "task_definition_family" {
-  value = "${aws_ecs_task_definition.td.family}"
+  value = "${aws_ecs_task_definition.main.family}"
 }
 
 output "name" {
   value = "${aws_ecs_service.main.name}"
 }
 
-output "iam_role" {
-  value = "${aws_ecs_service.main.iam_role}"
+output "iam_role_arn" {
+  value = "${aws_iam_role.main.arn}"
 }
